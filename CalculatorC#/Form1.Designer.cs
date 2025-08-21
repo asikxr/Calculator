b4601f0712ef2a47d25e87c12a7d90537690277c
asikxr <adrianmo0308@hotmail.com>
2025-08-21 00:16:17 -0600

    Agregar archivos de proyecto.

---
+++ CalculatorC#/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace CalculatorC_
+{
+    partial class MAIN
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MAIN));
+            this.SumarBtn = new System.Windows.Forms.Button();
+            this.IgualBtn = new System.Windows.Forms.Button();
+            this.MenosBtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.DivisionBtn = new System.Windows.Forms.Button();
+            this.ACBtn = new System.Windows.Forms.Button();
+            this.PorcentajeBtn = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btndot = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.DisplayTxb = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // SumarBtn
+            // 
+            this.SumarBtn.BackColor = System.Drawing.Color.DarkOrange;
+            resources.ApplyResources(this.SumarBtn, "SumarBtn");
+            this.SumarBtn.Name = "SumarBtn";
+            this.SumarBtn.UseVisualStyleBackColor = false;
+            // 
+            // IgualBtn
+            // 
+            this.IgualBtn.BackColor = System.Drawing.Color.DarkOrange;
+            resources.ApplyResources(this.IgualBtn, "IgualBtn");
+            this.IgualBtn.Name = "IgualBtn";
+            this.IgualBtn.UseVisualStyleBackColor = false;
+            // 
+            // MenosBtn
+            // 
+            this.MenosBtn.BackColor = System.Drawing.Color.DarkOrange;
+            resources.ApplyResources(this.MenosBtn, "MenosBtn");
+            this.MenosBtn.Name = "MenosBtn";
+            this.MenosBtn.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkOrange;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // DivisionBtn
+            // 
+            this.DivisionBtn.BackColor = System.Drawing.Color.DarkOrange;
+            resources.ApplyResources(this.DivisionBtn, "DivisionBtn");
+            this.DivisionBtn.Name = "DivisionBtn";
+            this.DivisionBtn.UseVisualStyleBackColor = false;
+            // 
+            // ACBtn
+            // 
+            this.ACBtn.BackColor = System.Drawing.Color.Gray;
+            resources.ApplyResources(this.ACBtn, "ACBtn");
+            this.ACBtn.ForeColor = System.Drawing.Color.Black;
+            this.ACBtn.Name = "ACBtn";
+            this.ACBtn.UseVisualStyleBackColor = false;
+            // 
+            // PorcentajeBtn
+            // 
+            this.PorcentajeBtn.BackColor = System.Drawing.Color.Gray;
+            resources.ApplyResources(this.PorcentajeBtn, "PorcentajeBtn");
+            this.PorcentajeBtn.ForeColor = System.Drawing.Color.Black;
+            this.PorcentajeBtn.Name = "PorcentajeBtn";
+            this.PorcentajeBtn.UseVisualStyleBackColor = false;
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.LightSlateGray;
+            resources.ApplyResources(this.btn7, "btn7");
+            this.btn7.ForeColor = System.Drawing.Color.Black;
+            this.btn7.Name = "btn7";
+            this.btn7.UseVisualStyleBackColor = false;
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.LightSlateGray;
+            resources.ApplyResources(this.btn4, "btn4");
+            this.btn4.ForeColor = System.Drawing.Color.Black;
+            this.btn4.Name = "btn4";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.LightSlateGray;
+            resources.ApplyResources(this.btn1, "btn1");
+            this.btn1.ForeColor = System.Drawing.Color.Black;
+            this.btn1.Name = "btn1";
+            this.btn1.UseVisualStyleBackColor = false;
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.LightSlateGray;
+            resources.ApplyResources(this.btn2, "btn2");
+            this.btn2.ForeColor = System.Drawing.Color.Black;
+            this.btn2.Name = "btn2";
+            this.btn2.UseVisualStyleBackColor = false;
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.LightSlateGray;
+            resources.ApplyResources(this.btn5, "btn5");
+            this.btn5.ForeColor = System.Drawing.Color.Black;
+            this.btn5.Name = "btn5";
+            this.btn5.UseVisualStyleBackColor = false;
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.LightSlateGray;
+            resources.ApplyResources(this.btn8, "btn8");
+            this.btn8.ForeColor = System.Drawing.Color.Black;
+            this.btn8.Name = "btn8";
+            this.btn8.UseVisualStyleBackColor = false;
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.LightSlateGray;
+            resources.ApplyResources(this.btn3, "btn3");
+            this.btn3.ForeColor = System.Drawing.Color.Black;
+            this.btn3.Name = "btn3";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.LightSlateGray;
+            resources.ApplyResources(this.btn6, "btn6");
+            this.btn6.ForeColor = System.Drawing.Color.Black;
+            this.btn6.Name = "btn6";
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.LightSlateGray;
+            resources.ApplyResources(this.btn9, "btn9");
+            this.btn9.ForeColor = System.Drawing.Color.Black;
+            this.btn9.Name = "btn9";
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // btndot
+            // 
+            this.btndot.BackColor = System.Drawing.Color.LightSlateGray;
+            resources.ApplyResources(this.btndot, "btndot");
+            this.btndot.ForeColor = System.Drawing.Color.Black;
+            this.btndot.Name = "btndot";
+            this.btndot.UseVisualStyleBackColor = false;
+            // 
+            // btn0
+            // 
+            this.btn0.BackColor = System.Drawing.Color.LightSlateGray;
+            resources.ApplyResources(this.btn0, "btn0");
+            this.btn0.ForeColor = System.Drawing.Color.Black;
+            this.btn0.Name = "btn0";
+            this.btn0.UseVisualStyleBackColor = false;
+            // 
+            // DisplayTxb
+            // 
+            resources.ApplyResources(this.DisplayTxb, "DisplayTxb");
+            this.DisplayTxb.Name = "DisplayTxb";
+            // 
+            // MAIN
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.DisplayTxb);
+            this.Controls.Add(this.btndot);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.PorcentajeBtn);
+            this.Controls.Add(this.ACBtn);
+            this.Controls.Add(this.DivisionBtn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MenosBtn);
+            this.Controls.Add(this.IgualBtn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.SumarBtn);
+            this.Name = "MAIN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SumarBtn;
+        private System.Windows.Forms.Button IgualBtn;
+        private System.Windows.Forms.Button MenosBtn;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button DivisionBtn;
+        private System.Windows.Forms.Button ACBtn;
+        private System.Windows.Forms.Button PorcentajeBtn;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btndot;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.TextBox DisplayTxb;
+    }
+}
+
---
+++ CalculatorC#/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorC_
+{
+    public partial class MAIN : Form
+    {
+        public MAIN()
+        {
+            InitializeComponent();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
